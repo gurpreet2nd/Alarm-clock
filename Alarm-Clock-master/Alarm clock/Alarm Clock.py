70390084609ade9fd93cf8d4d124f59ea951725a
gurpreet2nd <91679820+gurpreet2nd@users.noreply.github.com>
2021-10-01 19:17:32 +0530

    Update Alarm Clock.py

--- Alarm-Clock-master/Alarm clock/Alarm Clock.py
+++ Alarm-Clock-master/Alarm clock/Alarm Clock.py
@@ -41,11 +41,3 @@ entry2 = ttk.Entry(frame1, width=30)
 entry2.pack()
 
 button1 = ttk.Button(frame1, text="submit", command=SubmitButton)
-button1.pack()
-# this Label2 will show the Last Alarm Time
-label2 = ttk.Label(frame1)
-label2.pack()
-
-# label2.config(text="hello")
-
-root.mainloop()
